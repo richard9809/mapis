cec2596d484b940545ac90aa913bb444cfe3b07b
rmulu-dast <rmulu@DAST.CO>
2025-11-12 22:42:53 -0800

    Initial Commit

---
+++ src/main/java/com/personal/mapis/MapisApplication.java
@@ -0,0 +1,13 @@
+package com.personal.mapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapisApplication.class, args);
+	}
+
+}
